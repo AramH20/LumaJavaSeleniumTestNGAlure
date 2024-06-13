92967464220d41264825587b1047ad48022436e1
Aram_H <63616812+AramH20@users.noreply.github.com>
2024-06-12 21:19:21 -0700

    Update BagsPage.java

--- src/main/java/com/limatest/model/BagsPage.java
+++ src/main/java/com/limatest/model/BagsPage.java
@@ -12,7 +12,7 @@ public class BagsPage extends SideMenu{
         super(driver);
     }
 
-    @Step("Click {productName} Img.")
+    @Step("Click '{productName}' Img.")
     public ProductPage clickProductImg(String productName){
         String imgXpath = productNameXpath + productName + "']";
         getDriver().findElement(By.xpath(imgXpath)).click();
