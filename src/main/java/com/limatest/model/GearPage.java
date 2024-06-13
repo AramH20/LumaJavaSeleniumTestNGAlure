5d0b661e9ad6da8bbf17074ed43ee7c9473e3c00
Aram_H <63616812+AramH20@users.noreply.github.com>
2024-06-12 22:39:08 -0700

    Test product (#11)
    update yml file

---
+++ src/main/java/com/limatest/model/GearPage.java
@@ -0,0 +1,9 @@
+package com.limatest.model;
+
+import org.openqa.selenium.WebDriver;
+
+public class GearPage extends SideMenu{
+    public GearPage(WebDriver driver) {
+        super(driver);
+    }
+}
