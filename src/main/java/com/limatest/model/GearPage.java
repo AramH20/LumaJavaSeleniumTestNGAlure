3ce25305b239b4e05af3e8d73620924b4717cc9f
aharu <aharutyunyan2011@gmail.com>
2024-06-12 22:08:42 -0700

    update testproduct

--- src/main/java/com/limatest/model/GearPage.java
+++ src/main/java/com/limatest/model/GearPage.java
@@ -3,7 +3,6 @@ package com.limatest.model;
 import org.openqa.selenium.WebDriver;
 
 public class GearPage extends SideMenu{
-
     public GearPage(WebDriver driver) {
         super(driver);
     }
